236f7ac0658623f2d7e452af05a6dc2900c3f541
naiden00 <naiden.petrov.31.12.00@gmail.com>
2024-11-08 17:09:57 +0200

    create react app init

---
+++ smart_projects/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import {createBrowserRouter} from 'react-router-dom'
+
+const router = createBrowserRouter([{path: "/"}]);
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
