e3a269391c39a9f9dd98246419a6d83d6f24de8f
naiden00 <naiden.petrov.31.12.00@gmail.com>
2024-12-09 16:30:46 +0200

    Add Image Slider

--- smart_projects/src/app/pages/OurWorkPage.tsx
+++ smart_projects/src/app/pages/OurWorkPage.tsx
@@ -1,8 +1,9 @@
-import { Box } from '@mui/material';
+import { Box, Grid2 as Grid } from '@mui/material';
 
 import { Hero } from '../../components/ui/Heros/Hero';
-import { MasonryImageList } from '../../components/ui/Images/MasonryImageList';
+import Slider from '../../components/ui/Images/Slider';
 import ImagesWithTitleBar from '../../components/ui/Images/ImagesWithTitleBar';
+import { MasonryImageList } from '../../components/ui/Images/MasonryImageList';
 
 export const OurWorkPage = () => {
   return (
@@ -15,6 +16,18 @@ export const OurWorkPage = () => {
             from minor repairs to full-scale transformations, tailored to meet
             the unique needs of every client."
       ></Hero>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 2, sm: 8 }}
+        sx={{ ml: 2, mr: 2, mt: 2 }}
+      >
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid key={index} size={{ xs: 2, sm: 4, md: 4 }}>
+            <Slider />
+          </Grid>
+        ))}
+      </Grid>
       <ImagesWithTitleBar />
       <MasonryImageList />
     </Box>
--- smart_projects/src/components/ui/Images/Slider.tsx
+++ smart_projects/src/components/ui/Images/Slider.tsx
@@ -1,25 +1,64 @@
-import { Slide } from 'react-slideshow-image';
-
-import slideImages from '../../data/images';
+import { Slide, SlideProps } from 'react-slideshow-image';
 
 import 'react-slideshow-image/dist/styles.css';
 import styles from './Slider.module.css';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import {
+  Button,
+  Fab,
+  MobileStepper,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
+import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
+
+const properties = {
+  easing: 'ease',
+  indicators: true,
+  prevArrow: (
+    <Fab
+      size="small"
+      aria-label="scroll back to top"
+      title="Scroll to the top of the page"
+      color="secondary"
+      sx={{ ml: 1 }}
+    >
+      <KeyboardArrowLeftIcon color="inherit" />
+    </Fab>
+  ),
+  nextArrow: (
+    <Fab
+      size="small"
+      aria-label="scroll back to top"
+      title="Scroll to the top of the page"
+      color="secondary"
+      sx={{ mr: 1 }}
+    >
+      <KeyboardArrowRightIcon color="inherit" />
+    </Fab>
+  ),
+} as SlideProps;
 
 export default function Slider() {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
   return (
-    <div className={styles.container}>
-      <Slide easing="ease">
-        {slideImages.map((slide, index) => {
-          return (
-            <div className={styles.slide} key={slide}>
-              <div style={{ backgroundImage: `url(${slideImages[index]})` }}>
-                <span>Slide {index + 1}</span>
-              </div>
+    <Slide
+      arrows={isSmallScreen ? false : true}
+      transitionDuration={isSmallScreen ? 100 : 700}
+      {...properties}
+    >
+      {images.map((slide, index) => {
+        return (
+          <div className={styles.slide} key={slide}>
+            <div style={{ backgroundImage: `url(${images[index]})` }}>
+              <span>Slide {index + 1}</span>
             </div>
-          );
-        })}
-      </Slide>
-    </div>
+          </div>
+        );
+      })}
+    </Slide>
   );
 }
 
@@ -28,6 +67,4 @@ const images = [
   'https://cdn.pixabay.com/photo/2015/02/24/15/41/wolf-647528_960_720.jpg',
   'https://cdn.pixabay.com/photo/2021/01/21/15/54/books-5937716_960_720.jpg',
   'https://cdn.pixabay.com/photo/2015/04/23/21/59/tree-736875_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2014/05/03/00/56/summerfield-336672_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2015/03/17/14/05/sparkler-677774_960_720.jpg',
 ];
