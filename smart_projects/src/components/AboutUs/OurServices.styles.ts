891c49e2f1d352a724bca15a9ce222db16f63564
naiden00 <naiden.petrov.31.12.00@gmail.com>
2025-01-14 11:27:19 +0200

    .

--- smart_projects/src/components/AboutUs/OurServices.styles.ts
+++ smart_projects/src/components/AboutUs/OurServices.styles.ts
@@ -22,6 +22,7 @@ export const ourServicesStyles: OurServicesStyles = {
     borderTopRightRadius: 4,
     borderBottomLeftRadius: 4,
     borderBottomRightRadius: 4,
+    boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.8)',
   }),
   accordionDetails: (theme) => ({
     backgroundColor: theme.palette.secondary.light,
